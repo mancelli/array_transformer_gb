c7ea4d5b153011b096d2480907f780c58a566617
Mikhail Iagodin <dnshade19@gmail.com@>
2023-06-04 15:50:49 +0500

    started to program transformer

--- array_transformer/Program.cs
+++ array_transformer/Program.cs
@@ -1,2 +1,15 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите строку для добавления в массив");
+
+string[] initArray = new string[10];
+int count = 0;
+for(count = 0; count<initArray.Length; count++)
+{
+    initArray[count] = Console.ReadLine();
+    if (initArray[count] == String.Empty)
+    {
+        break;
+    }
+    System.Console.WriteLine("Введите следующую строку, либо нажмите ENTER чтобы завершить ввод");
+}
+
